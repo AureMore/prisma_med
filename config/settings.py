711d1cda3e81a21b372584ec48cc1a56e101abe5
Aurelio Moreno <dentisoftcustom@gmail.com>
2026-04-14 23:07:36 -0400

    correccion de nombres

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8q261^@0yzss3x+q6kbb1=wi0*2(xz7fz0%as@ux7gqwu_is=v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['prismamedsolutions.com', 'www.prismamedsolutions.com', '.vercel.app', 'localhost', '127.0.0.1']
+DEBUG = False
+ALLOWED_HOSTS = ['prismamedsolutions.com', 'www.prismamedsolutions.com', '.vercel.app']
 
 
 # Application definition
